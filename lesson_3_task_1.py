930db6cf98d6ded2b12cf32b5ba754a7e9ff6521
alexskypro <alexskypro@mail.ru>
2025-02-02 22:47:07 +0300

    Homework finished

---
+++ lesson_3_task_1.py
@@ -0,0 +1,5 @@
+from user import User
+my_user = User(input("Enter First name: "), input("Enter last name: "))
+print(my_user.get_first_name())
+print(my_user.get_last_name())
+print(my_user.get_all())
