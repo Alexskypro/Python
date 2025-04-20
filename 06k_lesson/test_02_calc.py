20f4760d8c5b6879edb1e5b2e746d0760d5cd120
alexskypro <alexskypro@mail.ru>
2025-04-21 00:06:42 +0300

    final_work

--- 06k_lesson/test_02_calc.py
+++ 06k_lesson/test_02_calc.py
@@ -1,30 +1,40 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.wait import WebDriverWait
+import pytest
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium import webdriver
 
 
-driver = webdriver.Chrome(service=ChromeService
-                          (ChromeDriverManager().install()))
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield driver
+    driver.quit()
 
 
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+@pytest.mark.usefixtures("driver")
+def test_screen(driver):
+    driver.get(
+        ("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+        )
+    waiter = WebDriverWait(driver, 50)
 
-waiter = WebDriverWait(driver, 50)
-delay = driver.find_element(By.CSS_SELECTOR, '#delay')
-delay.clear()
-delay.send_keys(45)
+    delay = driver.find_element(By.CSS_SELECTOR, '#delay')
 
-driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[1]').click()
-driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[4]').click()
-driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[2]').click()
-driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[15]').click()
-
-screen = driver.find_element(By.CSS_SELECTOR, 'div.screen')
-waiter.until(
+    delay.clear()
+    delay.send_keys('45')
+    sev = driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[1]')
+    sev.click()
+    pl = driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[4]')
+    pl.click()
+    ei = driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[2]')
+    ei.click()
+    sm = driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[15]')
+    sm.click()
+    screen = driver.find_element(By.CSS_SELECTOR, 'div.screen')
+    waiter.until(
         EC.text_to_be_present_in_element((By.CSS_SELECTOR, "div.screen"), "15")
     )
-assert screen.text == "15"
-driver.quit()
+    assert screen.text == "15"
+    driver.quit()
